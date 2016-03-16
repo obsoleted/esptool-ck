896e4fa48bcef2e1170fba105f21ab20136f65b5
Andre Price <obsoleted@users.noreply.github.com>
2016-03-16 09:29:25 -0700

    add windows run script

---
+++ test/runwin.cmd
@@ -0,0 +1,1 @@
+"../esptool.exe" -eo "eboot.elf" -bo "simplesample_httpasdfsdfsdfs.ino.bin" -bm qio -bf 40 -bz 512K -bs .text -bp 4096 -ec -eo "simplesample_httpasdfsdfsdfs.ino.elf" -bs .irom0.text -bs .text -bs .data -bs .rodata -bc -ec > output.txt
